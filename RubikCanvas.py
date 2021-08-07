7b45b9011dee3fc05b369c478958e0b295a8ed1e
Fernando Soto <fjsoto1199@gmail.com>
2021-08-06 21:28:30 -0500

    Removed some simple drawing methods since we are going to be using the 'move' approach

--- RubikCanvas.py
+++ RubikCanvas.py
@@ -37,10 +37,6 @@ class RubikCanvas(Canvas):
             self.curLastPos.y = e.y_root
 
     def onDrag(self, e):
-        # angle =
-        # print(angle)
-        # sign(self.curLastPos.y - e.y_root) *
-        # sign(self.curLastPos.x - e.x_root) *
         self.ytheta += tan((self.curLastPos.y - e.y_root) / SIDE_WIDTH)
         self.xtheta += tan((self.curLastPos.x - e.x_root) / SIDE_WIDTH)
         self.curLastPos.x = e.x_root
@@ -50,18 +46,32 @@ class RubikCanvas(Canvas):
         print(self.edge_points[0], e.x_root, e.y_root)
 
     # TODO: Investigate a possible fix for the perspective skew.
-    def draw_cube(self):
+    def draw_cube(self, rad=DOT_RAD):
         for i, point in enumerate(self.edge_points):
             _p = self.projected_point(point)
 
             # move instead of redraw to improve performance and memory usage
-            self.moveto(self.d_points[i], int(_p[0]), int(_p[1]))
+            self.coords(self.d_points[i], as_dot(_p))
 
-    def initialize_cube(self):
+            _prev_point = self.projected_point(self.edge_points[i - 1])
+
+            self.coords(self.d_lines[i], int(_p[0]), int(_p[1]), int(_prev_point[0]), int(_prev_point[1]))
+
+            # adjust for depth
+            if int(_p[2]) < 0:
+                self.tag_lower(self.d_points[i])
+            else:
+                self.tag_raise(self.d_points[i])
+
+    def initialize_cube(self, rad=DOT_RAD):
+        """
+        This method initializes the Rubik's cube drawing--necessary to avoid null-referencing.
+        """
         for i, point in enumerate(self.edge_points):
             _p = self.projected_point(point)
+
             # store drawing for deletion
-            self.d_points.append(self.draw_point(_p, color=DEBUG_CLRS[i], rad=5))
+            self.d_points.append(self.create_oval(as_dot(_p, rad=rad), fill=DEBUG_CLRS[i]))
 
             # account for depth
             if int(_p[2]) < 0:
@@ -69,7 +79,8 @@ class RubikCanvas(Canvas):
             else:
                 self.tag_raise(self.d_points[-1])
 
-            self.d_lines.append(self.draw_line(self.projected_point(self.edge_points[i - 1]), _p))
+            _p2 = self.projected_point(self.edge_points[i - 1])
+            self.d_lines.append(self.create_line(int(_p[0]), int(_p[1]), int(_p2[0]), int(_p2[1]), fill=OUT_CLR))
 
     def projected_point(self, p):
         _p = rot_x(p, self.ytheta)
@@ -78,41 +89,8 @@ class RubikCanvas(Canvas):
         _p = add(_p, matrix([[int(CENT_POINT[0])], [int(CENT_POINT[1])], [0]]))
         return _p
 
-    def draw_segment(self, p1, p2, rad=DOT_RAD):
-        """
-        This method draws a line segment from one point to the other.
 
-        :param p1: point one
-        :param p2: point two
-        :param rad: radius for the segment
-        """
-        return self.draw_point(p1, rad=rad), self.draw_point(p2, rad=rad), self.draw_line(p1, p2)
-
-    def draw_point(self, p, rad=DOT_RAD, color=OUT_CLR):
-        """
-        This method draws a point.
-
-        :param p: point to draw
-        :param rad: radius of the circle
-        :param color: color of the dot
-
-        :return: reference to the drawn point
-        """
-        return self.create_oval(as_dot(p, rad=rad), fill=color)
-
-    def draw_line(self, p1, p2):
-        """
-        This method draws a line.
-
-        :param p1: point one to start line
-        :param p2: point two to end line
-
-        :return: reference to the drawn line
-        """
-        return self.create_line(int(p1[0]), int(p1[1]), int(p2[0]), int(p2[1]), fill=OUT_CLR)
-
-
-def as_dot(p, rad=3):
+def as_dot(p, rad=DOT_RAD):
     return int(p[0]) - rad, int(p[1]) - rad, int(p[0]) + rad, int(p[1]) + rad
 
 
